d60feb0e0c20d9ea9a0ec8509c2749b8ee4efcdc
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-08 22:33:12 +0300

    Create 7.1understandingparameters.cs

---
+++ 7.1understandingparameters.cs
@@ -0,0 +1,24 @@
+using System;
+ 
+namespace Understanding_Parameter
+{
+  class Program
+   {
+     // function with parameter
+     public static int power(int num1)
+      {
+        int result;
+        result = num1 * num1;
+        return result;
+      }
+ 
+     static void Main(string[] args)
+      {
+        int pow;
+        // passing arguement as parameter
+        pow = Program.power(5);
+        Console.Write("\nPower = {0}", pow);
+        Console.ReadLine();
+      }
+   }
+}
