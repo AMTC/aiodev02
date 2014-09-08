3e4d351780f2494b08a36c3c5817c748bda7a1f0
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-08 22:54:20 +0300

    Create 9.15homework.cs

---
+++ 9.15homework.cs
@@ -0,0 +1,3 @@
+Qu1:Shows difference between checked statements and unchecked statements in C# using programming.
+
+Qu2:Create two classes and each class contains mathematical function to calculate number. Using return statement returns the value from the function.
