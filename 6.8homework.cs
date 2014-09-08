12ab1e3f91068f5d579d41540191490c18c707a8
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-08 22:29:58 +0300

    Create 6.8homework.cs

---
+++ 6.8homework.cs
@@ -0,0 +1,1 @@
+Qu1:: Write a program that calculate add, subtract, multiply and division. Accept two number from the users and then using appropriate function calculate them and shows output to the user.
