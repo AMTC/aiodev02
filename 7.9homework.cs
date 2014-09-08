c1f22a2dcd379df032c867faf60ea5d1f43cbc63
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-08 22:36:59 +0300

    Create 7.9homework.cs

---
+++ 7.9homework.cs
@@ -0,0 +1,5 @@
+Exercise 1: Explain difference between value type and reference type by writing a suitable C# example.
+
+Exercise 2: Write a program in which accept four numbers from the user and returns add, subtraction and multiplication of the value using out parameter.
+
+Exercise 3: Write a C# program to demonstrate command line argument. Accept 2 numbers from the user and calculate area and perimeter of rectangle.
