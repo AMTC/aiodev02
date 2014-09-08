b9345d71b4a62bdeab8b13c39200b09a76660c63
eugenmorar <eugenmorar@users.noreply.github.com>
2014-09-08 22:44:45 +0300

    Create 8.10homework.cs

---
+++ 8.10homework.cs
@@ -0,0 +1,6 @@
+Qu1:Write a program to explain protected access specifier.
+
+Qu2:Write a program to explain private access specifier.
+
+Qu3:Write a program to store and retrieve value from private variable using get set property.
+
